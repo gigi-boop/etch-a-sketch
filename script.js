67de8e627b163d1bd2504c1a7c247147404615b2
gigi-boop <agentile89@protonmail.com>
2022-10-26 00:57:46 -0400

    Add draw and reset functions

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+"use strict";
 
+const container = document.querySelector(".container");
+const resetButton = document.querySelector(".reset-button");
+
+let firstDraw = true;
+
+function reset() {
+    resetButton.addEventListener("click", () => {
+        let divs = document.querySelectorAll(".container > div");
+
+        for (const div of divs) {
+            container.removeChild(div);
+        }
+
+        firstDraw = false;
+
+        drawGrid();
+    });
+}
+
+function drawGrid() {
+    const containerWidth = container.offsetWidth;
+
+    let gridSize = 16;
+    if (firstDraw === false) {
+        gridSize = parseInt(
+            prompt("Please enter a grid size (max size 100)", "16")
+        );
+    }
+    let divWidth = Math.round((containerWidth / (gridSize + 1)) * 20) / 20;
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        let div = document.createElement("div");
+        container.appendChild(div);
+        div.style.width = `${divWidth}px`;
+    }
+}
+
+drawGrid();
+reset();
